58ba1f321580ad16a7e4eaceeb63e51ff1105c49
Aip <wjy7136@163.com>
2025-09-08 13:04:43 +0800

    feat:生成用户、数据分析相关类。 生成用户表（user）实体类，mapper接口，service接口； 生成数据分析表（chart）实体类，mapper接口，service接口；

---
+++ src/main/java/com/zhiyixi/mapper/ChartMapper.java
@@ -0,0 +1,20 @@
+package com.zhiyixi.mapper;
+
+import com.zhiyixi.model.entity.ChartDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author 93988
+* @description 针对表【chart(数据分析表)】的数据库操作Mapper
+* @createDate 2025-09-08 13:00:57
+* @Entity generator.domain.Chart
+*/
+@Mapper
+public interface ChartMapper extends BaseMapper<ChartDO> {
+
+}
+
+
+
+
