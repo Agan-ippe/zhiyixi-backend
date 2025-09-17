2b60bc9ca259d00908b1c903138b3ae51720bb50
Aip <wjy7136@163.com>
2025-09-17 17:18:02 +0800

    feat: 新增图表工具类

---
+++ src/main/java/com/zhiyixi/utils/ExcelUtils.java
@@ -0,0 +1,70 @@
+package com.zhiyixi.utils;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @Author <a href="https://github.com/Agan-ippe">知莫</a>
+ * @Date 2025/09/17   15:44
+ * @Version 1.0
+ * @Description excel工具类
+ */
+@Slf4j
+public class ExcelUtils {
+    /**
+     * excel 转换为 csv
+     * @param multipartFile
+     * @return
+     */
+    public static String excelToCsv(MultipartFile multipartFile){
+        // 读取数据
+        List<Map<Integer,String>> list = null;
+        try {
+            list = EasyExcel.read(multipartFile.getInputStream())
+                    .excelType(ExcelTypeEnum.XLSX)
+                    .sheet()
+                    .headRowNumber(0)
+                    .doReadSync();
+        } catch (IOException e) {
+            log.error("读取excel文件失败",e);
+        }
+        if (CollUtil.isEmpty(list)) {
+            log.error("excel文件内容为空");
+            return "";
+        }
+        // 转换为csv
+        StringBuilder sb = new StringBuilder();
+        // 读取表头
+        LinkedHashMap<Integer, String> headMap = (LinkedHashMap<Integer, String>) list.get(0);
+        // 过滤
+        List<String> headList = filterDateIsNull(headMap);
+        sb.append(StrUtil.join(",", headList)).append("\n");
+        // 读取数据
+        for (int i = 1; i < list.size(); i++) {
+            LinkedHashMap<Integer, String> dataMap = (LinkedHashMap<Integer, String>) list.get(i);
+            // 过滤
+            List<String> dataList = filterDateIsNull(dataMap);
+            sb.append(StrUtil.join(",", dataList)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 过滤数据是 null 的数据
+     * @param targetMap
+     * @return
+     */
+    static List<String> filterDateIsNull(LinkedHashMap<Integer,String> targetMap){
+        return targetMap.values().stream().filter(StrUtil::isNotBlank).collect(Collectors.toList());
+    }
+}
