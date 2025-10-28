97f81edc6f01db24a1573da94ab72d4b9fb7b757
Aip <wjy7136@163.com>
2025-10-28 17:18:58 +0800

    test(AiManager):星火Ai服务的测试类

--- src/test/java/com/zhiyixi/manager/AiManagerTest.java
+++ src/test/java/com/zhiyixi/manager/AiManagerTest.java
@@ -5,8 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @Author <a href="https://github.com/Agan-ippe">知莫</a>
  * @Date 2025/09/26   18:22
@@ -30,9 +28,9 @@ class AiManagerTest {
                 "分析网站用户的增长情况\n" +
                 "原始数据：\n" +
                 "日期,用户数\n" +
-                "1号,10\n" +
-                "2号,20\n" +
-                "3号,30\n");
+                "1号,86\n" +
+                "2号,132\n" +
+                "3号,97\n");
         System.out.println(answer);
     }
 }
